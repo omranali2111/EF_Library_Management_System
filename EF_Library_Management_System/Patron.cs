5ccc1a010dc27fdcc6fdd0090dceebcbc7b262f5
omranali2111 <aljabri1234@hotmail.com>
2023-09-24 12:07:24 +0400

    adding class for patron

---
+++ EF_Library_Management_System/Patron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Library_Management_System
+{
+    internal class Patron
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string ContactNumber { get; set; }
+
+    }
+}
