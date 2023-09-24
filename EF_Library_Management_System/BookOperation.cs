eea111aa54b2d39a40e93551147a5f3d211ee458
omranali2111 <aljabri1234@hotmail.com>
2023-09-24 13:06:09 +0400

    adding method RemoveBook

--- EF_Library_Management_System/BookOperation.cs
+++ EF_Library_Management_System/BookOperation.cs
@@ -11,8 +11,14 @@ namespace EF_Library_Management_System
     {
         public void AddBook(string title, string author, int publicationYear)
         {
+           
             using (var dbContext = new LibraryDBContext())
             {
+                if (dbContext.Books.Any(b => b.Title == title && b.Author == author))
+                {
+                    Console.WriteLine("The book with the same title and author already exists in the library.");
+                    return;
+                }
                 var newBook = new Book
                 {
                     Title = title,
@@ -25,6 +31,25 @@ namespace EF_Library_Management_System
                 dbContext.SaveChanges();
 
                 Console.WriteLine("Book added successfully to the library.");
+            }
+        }
+        public void RemoveBook(string title)
+        {
+            using (var dbContext = new LibraryDBContext())
+            {
+                var bookToRemove = dbContext.Books.FirstOrDefault(book => book.Title == title);
+                if (bookToRemove != null)
+                {
+                    // Remove the book from the context and save changes
+                    dbContext.Books.Remove(bookToRemove);
+                    dbContext.SaveChanges();
+                    Console.WriteLine($"Book '{title}' has been removed from the library.");
+                }
+                else
+                {
+                    Console.WriteLine($"Book '{title}' was not found in the library.");
+                }
+
             }
         }
     }
