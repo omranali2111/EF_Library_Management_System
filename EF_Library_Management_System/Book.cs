6da45ae8e30db2031ae9ca24bda2e1aeffb67f6a
omranali2111 <aljabri1234@hotmail.com>
2023-09-24 12:04:11 +0400

    adding class book

---
+++ EF_Library_Management_System/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_Library_Management_System
+{
+    internal class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Author { get; set; }
+
+        [Required]
+      
+        public int PublicationYear { get; set; }
+        public bool IsAvailable { get; set; } 
+
+    }
+}
