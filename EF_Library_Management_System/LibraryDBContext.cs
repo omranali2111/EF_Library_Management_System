ee7ebb284b2bc0e3d52182ff08ec309c74feb1f4
omranali2111 <aljabri1234@hotmail.com>
2023-09-24 11:59:06 +0400

    creating EF_Library_Management_System using EFCore

---
+++ EF_Library_Management_System/LibraryDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Library_Management_System
+{
+    internal class LibraryDBContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer("Data Source=(local);Initial Catalog=EFCoreLibrary; Integrated Security=true; TrustServerCertificate=True");
+
+        }
+    }
+}
